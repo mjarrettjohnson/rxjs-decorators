e4fdcdbe5437201e45c8ced35deff01c16f1f220
Miles <mjarrettjohnson@gmail.com>
2018-07-31 22:11:26 +1000

    rebuild

--- src/dist/index.d.ts
+++ src/dist/index.d.ts
@@ -1,14 +1,2 @@
-import { Observable } from 'rxjs';
-import { ReactiveModel } from './src/decorators';
-export declare class TestClass extends ReactiveModel {
-    counter$: Observable<number>;
-    counter$2: Observable<number>;
-    multiples$: Observable<number>;
-    combined$: any;
-    combined2$: any;
-    private sideData;
-    constructor();
-    print(x: number): void;
-    multiply(x: any): void;
-}
+export * from './src/decorators';
 //# sourceMappingURL=index.d.ts.map
